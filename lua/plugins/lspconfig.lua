bf1081fb49a318345b7f79d0a0f29f2bedb4ec4c
Alessandro Zolli <personal@alessandrozolli.com>
2025-08-30 23:15:59 +0200

    Added typescript lsp support

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,31 +1,33 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-        -- Setup python support
-        require("lspconfig").pyright.setup{}
-        -- Setup lua support (install lua-language-server first!)
-        require("lspconfig").lua_ls.setup{
-            settings = {
-                Lua = {
-                    runtime = {
-                        version = 'LuaJIT',
-                    },
-                    diagnostics = {
-                        globals = {'vim'},
-                    },
-                    workspace = {
-                        library = vim.api.nvim_get_runtime_file("", true),
-                        checkThirdParty = false,
-                    },
-                    telemetry = {
-                        enable = false,
-                    },
-                },
-            },
-        }
-        -- Setup c support (install clangd first!)
-        require("lspconfig").clangd.setup{}
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			-- Setup python support
+			require("lspconfig").pyright.setup({})
+			-- Setup lua support (install lua-language-server first!)
+			require("lspconfig").lua_ls.setup({
+				settings = {
+					Lua = {
+						runtime = {
+							version = "LuaJIT",
+						},
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+							checkThirdParty = false,
+						},
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
+			})
+			-- Setup c support (install clangd first!)
+			require("lspconfig").clangd.setup({})
+			-- Setup typescript support (install ts-language-server first!)
+			require("lspconfig").tsserver.setup({})
+		end,
+	},
 }
